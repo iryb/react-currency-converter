2e3191fdf6e0cadaf5b0ad0ce2f55110bd29b518
Iryb <iryna.burianovata@gmail.com>
2024-09-19 15:33:03 +0300

    Add test

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Header } from "./Header";
 import { fetchRates } from "../services/api";
+import { getExchangePrice } from "../lib/utils";
 
 jest.mock("../services/api", () => ({
   fetchRates: jest.fn(),
@@ -21,4 +22,25 @@ test("loads and displays exchange rate", async () => {
   expect(usdLabel).toBeInTheDocument();
   const eurLabel = await screen.findByText("EUR");
   expect(eurLabel).toBeInTheDocument();
+});
+
+test("displays correct exchange rate", async () => {
+  const rates = {
+    selectedRates: {
+      USD: 1.2,
+      EUR: 0.9,
+    },
+  };
+
+  (fetchRates as jest.Mock).mockResolvedValue(rates);
+
+  render(<Header />);
+
+  const expectedUsdValue = getExchangePrice(rates.selectedRates.USD);
+  const usdValue = await screen.findByText(expectedUsdValue);
+  expect(usdValue).toBeInTheDocument();
+
+  const expectedEurValue = getExchangePrice(rates.selectedRates.EUR);
+  const eurValue = await screen.findByText(expectedEurValue);
+  expect(eurValue).toBeInTheDocument();
 });
